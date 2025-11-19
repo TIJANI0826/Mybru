10eb71f566147f5f999e6fba09e8843555fce153
TIJANI0826 <ibrahimtijani08@gmail.com>
2025-11-19 11:04:18 +0100

    Add mybru.orender.com to ALLOWED_HOSTS

--- backend/mybrutea_backend/settings.py
+++ backend/mybrutea_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pleasereplacethiswitharealsecretkey'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mybru.onrender.com","127.0.0.1","localhost"]
 
 
 # Application definition
