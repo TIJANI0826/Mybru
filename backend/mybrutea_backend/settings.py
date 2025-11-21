34d065ebcdfd790723d0d962eb7e691b0a78afa9
TIJANI0826 <ibrahimtijani08@gmail.com>
2025-11-21 19:05:34 +0100

    add another Allowed host

--- backend/mybrutea_backend/settings.py
+++ backend/mybrutea_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pleasereplacethiswitharealsecretkey'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mybru.onrender.com","127.0.0.1","localhost"]
+ALLOWED_HOSTS = ["mybru.onrender.com","127.0.0.1","localhost", "tjib26.pythonanywhere.com"]
 
 
 # Application definition
